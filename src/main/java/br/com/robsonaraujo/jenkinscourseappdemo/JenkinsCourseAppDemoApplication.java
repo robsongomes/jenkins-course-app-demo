af6aae4e0cada41c9a289b4939a27a8d4378b436
robson <nosbor84@gmail.com>
2018-08-25 10:10:01 -0300

    Initial commit

---
+++ src/main/java/br/com/robsonaraujo/jenkinscourseappdemo/JenkinsCourseAppDemoApplication.java
@@ -0,0 +1,12 @@
+package br.com.robsonaraujo.jenkinscourseappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCourseAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCourseAppDemoApplication.class, args);
+	}
+}
