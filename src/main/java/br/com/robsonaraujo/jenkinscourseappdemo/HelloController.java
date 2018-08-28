eaa9e43555b756c4577843dca90f94ab6a891919
robson <nosbor84@gmail.com>
2018-08-27 21:20:13 -0300

    /hello endpoint criado

---
+++ src/main/java/br/com/robsonaraujo/jenkinscourseappdemo/HelloController.java
@@ -0,0 +1,13 @@
+package br.com.robsonaraujo.jenkinscourseappdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
